aea0c307e4cd8b10ed53b9e5bae989ff6b4933cf
VN-Azura <abc@gmail.com>
2022-10-27 09:06:01 +0700

    ok

--- Backend/src/CRUD/Info_Customer.js
+++ Backend/src/CRUD/Info_Customer.js
@@ -1,12 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
 import { useDispatch, useSelector } from "react-redux";
 import { getList } from "../kAcctions/Subcribe";
 import { Link } from "react-router-dom";
+import { DataGrid } from "@mui/x-data-grid";
 import Sidebar from "../components/sidebar/Sidebar";
 import Navbar from "../components/navbar/Navbar";
-import { DataGrid } from "@mui/x-data-grid";
 import "./Info_Customer.scss";
+
 const SubscribePage = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const dispatch = useDispatch();
   const subscribe = useSelector((state) => state.Subscribe);
   console.log("res", subscribe.list);
@@ -70,10 +85,13 @@ const SubscribePage = () => {
             Quản lý Subscribe
           </div>
           <div className="d-flex flex-row mb-3">
-            <button className="btn btn-primary ml-3 ">add</button>
+            <button className="btn btn-primary ml-3 " onClick={handleClickOpen}>
+              add
+            </button>
             <button className="btn btn-primary ml-3 ">edit</button>
             <button className="btn btn-danger ml-3">delete</button>
           </div>
+
           <DataGrid
             className="datagrid"
             autoHeight
@@ -84,6 +102,78 @@ const SubscribePage = () => {
             rowsPerPageOptions={[9]}
             checkboxSelection
           />
+
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+              style: {
+                width: "100%",
+              },
+            }}
+          >
+            <DialogTitle>Thêm Subscribe</DialogTitle>
+
+            <DialogContent>
+              <form>
+                <div className="row ">
+                  <div>
+                    <div className="form-group ">
+                      <label className="required">Name</label>
+                      <input
+                        type="text"
+                        className="form-control "
+                        // ref={post_name}
+                      ></input>
+                    </div>
+
+                    <div className="form-group " style={{ marginTop: 20 }}>
+                      <label className="required">Email</label>
+                      <input
+                        type="text"
+                        className="form-control "
+                        // ref={post_title}
+                      ></input>
+                    </div>
+
+                    <div className="form-group " style={{ marginTop: 20 }}>
+                      <label className="required">Message</label>
+                      <textarea
+                        type="text"
+                        className="form-control "
+                        style={{
+                          height: "100px",
+                        }}
+                        // ref={post_image}
+                      ></textarea>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </DialogContent>
+
+            <DialogActions>
+              <div className="button">
+                <button
+                  // onClick={createPost}
+                  type="reset"
+                  value="Reset"
+                  className="btn btn-primary text-center"
+                >
+                  Thêm
+                </button>
+                <button
+                  onClick={() => setOpen(false)}
+                  className="btn btn-danger"
+                  type="reset"
+                  value="Reset"
+                  style={{ marginLeft: 20 }}
+                >
+                  Đóng
+                </button>
+              </div>
+            </DialogActions>
+          </Dialog>
         </div>
       </div>
     </div>
